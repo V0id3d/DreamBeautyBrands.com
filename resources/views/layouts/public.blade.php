6bd5d0c769bfb624262360067a9647d89bee812f
Carlos Quinones <CQuinones@Qzion.net>
2016-10-05 13:36:49 -0400

    Initial Setup Set Debug Bar and Ide Helper Up to only work in dev mode

---
+++ resources/views/layouts/public.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Hair Care Products Branding">
+    <meta name="author" content="Carlos Quinones">
+
+    <title>Dream Beauty Brands</title>
+
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+</html>
