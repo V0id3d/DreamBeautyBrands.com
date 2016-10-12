d48f0e362725fe3e9bb995a0a7b9f08e8bfbf2e3
Carlos Quinones <CQuinones@Qzion.net>
2016-10-12 12:21:49 -0400

    Initial Setup Cleaned Up SaSS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,15 +5,16 @@
 @endsection
 
 @section('PageContents')
-    <div class="sectionHeader">
-        <div class="container">
-            <h1>Helping to make your dream project a reality</h1>
+    <section>
+        <div class="section-header">
+            <div class="container">
+                <h1>Helping to make your dream project a reality</h1>
+            </div>
         </div>
-    </div>
-    <div id="section1">
         <div class="container">
             <div class="row">
-                <section class="col-md-5 section-text">
+                {{--Section Text--}}
+                <div class="col-md-5 section-text">
                     <p>
                         You can be as involved as you like with the creation of your products. We have perfected
                         formulations ready to go, which will be packaged under your brand/salon name and can be on your
@@ -25,33 +26,9 @@
                         Be inspired. Be creative. Think of something new. Think of something unique. Your ideas are the
                         inspiration for you creations.
                     </blockquote>
-                    {{--<p>--}}
-                        {{--Like yourself, we work in the hair and beauty industry. We have worked with all the big names--}}
-                        {{--and we have seen a substantial amount of change in the way the industry works over recent years.--}}
-                        {{--There is little respect given to professional products any more and I’m sure you’ll agree, times--}}
-                        {{--are tougher now than they have ever been. We have to contend with online prices, no restrictions--}}
-                        {{--on professional only products and there now there is left business left for the hair salons. The--}}
-                        {{--product ranges have become so big and mass market, they are almost unaffordable to stock and the--}}
-                        {{--salon support just isn’t there anymore.--}}
-                    {{--</p>--}}
-
-                    {{--<p>--}}
-                        {{--Fortunately, I had the opportunity to meet Juan. He owns a beauty laboratory in the USA and he--}}
-                        {{--has to be one of the most knowledgeable and helpful people I’ve ever met. He introduced me to a--}}
-                        {{--whole different side of the industry, manufacturing. The chemistry of how the products are--}}
-                        {{--created and the ethics that should be installed in creating high quality formulations. He creates--}}
-                        {{--true professional products, made without any compromise on ingredients and formulated to create--}}
-                        {{--true professional results.--}}
-                    {{--</p>--}}
-
-                    {{--<p>--}}
-                        {{--Together, Juan and I came up with the idea of promoting this concept directly to the salons--}}
-                        {{--having the same issues I had. Lets work together to create products stylists can trust and--}}
-                        {{--confidently recommend to their clients. Products that reflect the true meaning of salon sell--}}
-                        {{--through products. Most importantly products that reflect and represent your salon and your brand!--}}
-                    {{--</p>--}}
-                </section>
-                <section class="col-md-7">
+                </div>
+                {{--Info Graphic--}}
+                <div class="col-md-7">
                     <h1>How It Works</h1>
                     <div class="infoGraphic" id="howItWorks">
                         <div class="infoGraphic-inner">
@@ -166,76 +143,55 @@
                             </div>
                         </div>
                     </div>
-                </section>
+                </div>
             </div>
         </div>
-    </div>
-
-    <div class="sectionHeader">
-        <div class="container">
-            <h1>Why Have Your Own Products?</h1>
+    </section>
+    <section>
+        <div class="section-header">
+            <div class="container">
+                <h1>Why Have Your Own Products?</h1>
+            </div>
         </div>
-    </div>
-    <div id="section3">
         <div class="container">
             <div class="row">
-                <section>
-                    <div class="col-md-6">
+                <div class="col-md-6">
 
-                    </div>
-                    <div class="col-md-6">
-                        <ul>
-                            <li>
-                                Why not? You’re already supplying products through your salon. Your own branded products are
-                                likely to increase sales and increase revenue. Why promote other peoples brands, when you
-                                can promote your own?
-                            </li>
-                            <li>
-                                You have a name that your clients trust. Everyone trusts their hairdresser, with a client in
-                                your chair they are excited to be introduced and educated on your products, the products
-                                your using on their hair.
-                            </li>
-                            <li>
-                                You will gain a higher client retention. If your client knows you have the best product for
-                                their hair, they simply will have to come back to you to buy it!
-                            </li>
-                            <li>
-                                No internet competition! Its hard when you are trying to sell products to your clients who
-                                trust you, when you know they are able to purchase them online cheaper. This is a breach of
-                                your trust, and since most of us treat clients like friends, is a hard thing to do resulting
-                                in less salon sales.
-                            </li>
-                            <li>
-                                Endless sale or promotional opportunities. You’re in control, you know your clients and the
-                                way your salon works the best so create your offers directly for your customers.
-                            </li>
-                            <li>
-                                No restrictions on re-sale. This is your brand name and you can sell your products wherever
-                                and however you like. If you chose to promote your products online through a webstore or
-                                just on social media, this is entirely your choice.
-                            </li>
-                        </ul>
-                    </div>
-
-                    {{--<p>--}}
-                        {{--Why not? You’re already supplying products through your salon. Your own branded products are likely to--}}
-                        {{--increase sales and increase revenue. Why promote other peoples brands, when you can promote your own?--}}
-                        {{--You have a name that your clients trust. Everyone trusts their hairdresser, with a client in your chair--}}
-                        {{--they are excited to be introduced and educated on your products, the products your using on their hair.--}}
-                        {{--You will gain a higher client retention. If your client knows you have the best product for their hair,--}}
-                        {{--they simply will have to come back to you to buy it! No internet competition! Its hard when you are--}}
-                        {{--trying to sell products to your clients who trust you, when you know they are able to purchase them--}}
-                        {{--online cheaper. This is a breach of your trust, and since most of us treat clients like friends, is a--}}
-                        {{--hard thing to do resulting in less salon sales. Endless sale or promotional opportunities. You’re in--}}
-                        {{--control, you know your clients and the way your salon works the best so create your offers directly for--}}
-                        {{--your customers. No restrictions on re-sale. This is your brand name and you can sell your products--}}
-                        {{--wherever and however you like. If you chose to promote your products online through a webstore or just--}}
-                        {{--on social media, this is entirely your choice.--}}
-                    {{--</p>--}}
-
-                </section>
+                </div>
+                <div class="col-md-6">
+                    <ul>
+                        <li>
+                            Why not? You’re already supplying products through your salon. Your own branded products are
+                            likely to increase sales and increase revenue. Why promote other peoples brands, when you
+                            can promote your own?
+                        </li>
+                        <li>
+                            You have a name that your clients trust. Everyone trusts their hairdresser, with a client in
+                            your chair they are excited to be introduced and educated on your products, the products
+                            your using on their hair.
+                        </li>
+                        <li>
+                            You will gain a higher client retention. If your client knows you have the best product for
+                            their hair, they simply will have to come back to you to buy it!
+                        </li>
+                        <li>
+                            No internet competition! Its hard when you are trying to sell products to your clients who
+                            trust you, when you know they are able to purchase them online cheaper. This is a breach of
+                            your trust, and since most of us treat clients like friends, is a hard thing to do resulting
+                            in less salon sales.
+                        </li>
+                        <li>
+                            Endless sale or promotional opportunities. You’re in control, you know your clients and the
+                            way your salon works the best so create your offers directly for your customers.
+                        </li>
+                        <li>
+                            No restrictions on re-sale. This is your brand name and you can sell your products wherever
+                            and however you like. If you chose to promote your products online through a webstore or
+                            just on social media, this is entirely your choice.
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
-
+    </section>
 @endsection
