e4b06f0d2d8ef8cc23b7ebbafe24dc392585a47e
Carlos Quinones <Cquinones@qzion.net>
2017-11-20 14:33:45 -0500

    Updated Product Info As Per Robert Riley

--- resources/views/public/products/styling.blade.php
+++ resources/views/public/products/styling.blade.php
@@ -14,62 +14,44 @@
     <section class="gradient-down">
         <div class="section-header">
             <div class="container">
-                <h1>Conditioning Hair Spray</h1>
+                <h1>Styling Products</h1>
             </div>
         </div>
         <div class="container">
             <div class="row">
-                <div class="col-md-12" style="margin-bottom: 25px;">
+                <div class="col-md-4">
+                    <h2>Hair Repair Creme</h2>
                     <p>
-                        This lightweight and workable finishing hair spray covers the hair in a reflective shield of
-                        shine that helps to fights fizz and humidity, yet brushes out easily and leaves no flakey or
-                        sticky residue. Infused with a combination of advanced ingredients, including antioxidant-rich
-                        argan oil. Available in two types of hold
+                        You think you’ve seen amazing leave-in creme, but our formulation is truly best in show. It 
+                        will become your BFF for expert, gorgeous styling.  Not only does the styling creme enable 
+                        you to effectively style even the thickest, textured hair, the added benefits of moisturisation 
+                        and heat protectant enhance the health of your client’s hair. The crème helps the hair retain 
+                        its memory, which makes your job much easier. The essential oils hydrate and nourish hair 
+                        from the roots to the ends, while the citrus and protein extracts stimulate the scalp, resulting 
+                        in vibrant, selfie-worthy hair.  
                     </p>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <h2>Medium Hold Shine Spray</h2>
+                <div class="col-md-4">
+                    <h2>Hair Repair Spray</h2>
                     <p>
-                        This medium hold, flexible hairspray provides long-lasting, weightless hold for soft and
-                        natural styles. Medium hold is ideal for natural styles, including easy up-dos, taming flyaway
-                        and fixing blowouts.
+                        Lightweight, moisturising and protective, this multi-purpose mist is one of a kind. Created to deliver 
+                        moisture directly to the hair strand, smoothing and adding a defensive shield to protect the hair from 
+                        environmental factors (including ultraviolet light) as well as heat styling. This barrier helps hair to 
+                        retain internal moisture and provides a smooth finish. Hair is left silky, shiny and better able to 
+                        repel the effects of humidity. Designed with a lightweight formulation that combines proteins, aloe vera, 
+                        fruit and citrus extracts for hair and scalp vitality and thermal protection, it doesn’t weigh down the 
+                        hair allowing you to create volume or finish curls perfectly, providing long lasting frizz control.   
                     </p>
                 </div>
-            </div>
-        </div>
-    </section>
-    <section>
-        <div class="section-header">
-            <div class="container">
-                <h1>Additional</h1>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <h2>Leave In Creme</h2>
+                <div class="col-md-4">
+                    <h2>Styling Foam</h2>
                     <p>
-                        Intensive, smoothing and protective the ideal partner for achieving a perfect blow-dry. An
-                        intensive, lightweight formulation that instantly boosts moisture and elasticity to leave a
-                        unique radiance to hair. It also provides a protective shield around every hair strand to help
-                        protect hair from heat and environmental damage, eliminate frizz, reduce static and repel
-                        humidity. Use with even the most unruly types of hair to smooth, detangle and style with ease
-                        creating the perfect finish every time.
+                        This lightweight styling foam leaves no residue and protects from both heat and sun with UV filtering. 
+                        Fruit and citrus extracts hydrate scalp and hair while the aloe vera reduces inflammation and balances 
+                        hair. This is a great product that protects hair and helps you more effectively style. 
                     </p>
                 </div>
-                <div class="col-md-6">
-                    <h2>Leave In Spray</h2>
-                    <p>
-                        Intensive, smoothing and protective the ideal partner for achieving a perfect blow-dry. An
-                        intensive, lightweight formulation that instantly boosts moisture and elasticity to leave a
-                        unique radiance to hair. It also provides a protective shield around every hair strand to help
-                        protect hair from heat and environmental damage, eliminate frizz, reduce static and repel
-                        humidity. Use with even the most unruly types of hair to smooth, detangle and style with ease
-                        creating the perfect finish every time.
-                    </p>
-                </div>
+                
             </div>
         </div>
     </section>
